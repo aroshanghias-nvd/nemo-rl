698e4497eeb26e42b59ce231da29ffbe828202a8
Jarno Seppänen <jseppanen@nvidia.com>
2025-09-19 15:30:36 +0300

    Guard against no valid samples

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -845,7 +845,7 @@ def grpo_train(
 
         timing_metrics: dict[str, float] = timer.get_timing_metrics(reduction_op="sum")  # type: ignore
         # track example with high token mult prob error above 1.05
-        if metrics["token_mult_prob_error"] > 1.05:
+        if metrics.get("token_mult_prob_error", 0) > 1.05:
             logger.log_plot_token_mult_prob_error(
                 {
                     "prompt_lengths": repeated_batch["length"],
