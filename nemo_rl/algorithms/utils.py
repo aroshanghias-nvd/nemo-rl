9e7b9540ba618962d110c9cec6529e27154af79f
Jarno Seppänen <jseppanen@nvidia.com>
2025-10-10 15:12:24 +0300

    fix: Use custom chat template also in VLM's
    Fix policy.tokenizer.chat_template config to have effect in VLM's
    
    Signed-off-by: Jarno Seppänen <jseppanen@nvidia.com>

--- nemo_rl/algorithms/utils.py
+++ nemo_rl/algorithms/utils.py
@@ -244,9 +244,15 @@ def get_tokenizer(
         tokenizer.pad_token = tokenizer.eos_token
 
     if "chat_template" in tokenizer_config:
+
+        def set_chat_template(chat_template: str):
+            tokenizer.chat_template = chat_template
+            if processor is not None:
+                processor.chat_template = chat_template
+
         if tokenizer_config["chat_template"] is None:
             print("Using passthrough chat template")
-            tokenizer.chat_template = COMMON_CHAT_TEMPLATES.passthrough_prompt_response
+            set_chat_template(COMMON_CHAT_TEMPLATES.passthrough_prompt_response)
         elif tokenizer_config["chat_template"].lower() == "default":
             print("Using tokenizer's default chat template")
         elif tokenizer_config["chat_template"].endswith(".jinja"):
@@ -254,10 +260,10 @@ def get_tokenizer(
             template_path = tokenizer_config["chat_template"]
             print(f"Loading chat template from file: {template_path}")
             with open(template_path, "r") as f:
-                tokenizer.chat_template = f.read()
+                set_chat_template(f.read())
         else:
             print("Using custom chat template")
-            tokenizer.chat_template = tokenizer_config["chat_template"]
+            set_chat_template(tokenizer_config["chat_template"])
     else:
         print("No chat template provided, using tokenizer's default")
 
