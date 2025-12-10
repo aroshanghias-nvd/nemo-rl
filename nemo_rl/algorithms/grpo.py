bf401e755caac86f4b5bf133f9c72d4c86033d80
Yi-Fu Wu <yifu.wu@gmail.com>
2025-12-09 16:08:56 -0800

    Cherry pick gym-related nano-v3 changes
    Co-authored-by: Peter Jin <pjin@nvidia.com>
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>
    (cherry picked from commit a9beac31118acb64e60aa48a52cb6c7ce03d9850)

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -1617,15 +1617,22 @@ def grpo_train(
 
             # Logging
             # Log training data
-            log_data = {"content": flat_messages["content"]}
+            log_data = {}
+            if "agent_ref" in repeated_batch:
+                log_data["agent_ref"] = repeated_batch["agent_ref"]
+            log_data["content"] = flat_messages["content"]
             log_data["rewards"] = rewards.tolist()
             if master_config["grpo"]["use_dynamic_sampling"]:
                 log_data["filtered_rewards"] = rewards.tolist()
                 log_data["rewards"] = repeated_batch["total_reward"].tolist()
-
+            log_data["input_lengths"] = input_lengths.tolist()
+            log_data["token_ids"] = train_data["input_ids"].tolist()
+            log_data["token_loss_mask"] = train_data["token_mask"].tolist()
+            log_data["sample_loss_mask"] = train_data["sample_mask"].tolist()
+            log_data["advantages"] = train_data["advantages"].tolist()
             log_data["generation_logprobs"] = train_data["generation_logprobs"].tolist()
             log_data["prev_logprobs"] = train_data["prev_logprobs"].tolist()
-            log_data["input_lengths"] = input_lengths.tolist()
+
             logger.log_batched_dict_as_jsonl(
                 log_data, f"train_data_step{total_steps + 1}.jsonl"
             )
